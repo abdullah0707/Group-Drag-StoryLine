158ba599bf42dd6fe4ae4918dfe39d8ed6f803b0
Abdullah Mohammed <abdullahelgammal25@gmail.com>
2024-09-27 13:57:37 +0300

    up 1

---
+++ story_content/triggers.js
@@ -0,0 +1,45 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6JSgFo0gD7e":
+        Script1();
+        break;
+      case "6rDb5ywlZH3":
+        Script2();
+        break;
+      case "6HO7W0HOdoR":
+        Script3();
+        break;
+      case "5elY86SZTpj":
+        Script4();
+        break;
+      case "6Phoa8fiaeL":
+        Script5();
+        break;
+      case "6SUzKnKU99P":
+        Script6();
+        break;
+      case "6EynGJz6D3u":
+        Script7();
+        break;
+      case "60pVlSq0Mxi":
+        Script8();
+        break;
+      case "5rxwFAU8f8l":
+        Script9();
+        break;
+      case "6IvhRkEevm5":
+        Script10();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
